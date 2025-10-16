e294a2877343c9e62b59a66a131149ddd35fc08c
yuzhakov <yuzhakov.a.a@mail.ru>
2025-10-16 11:21:44 +0500

    refinement function widget

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,12 @@
-import re
 from typing import Union
 
 def get_mask_card_number(card_number: Union[int]) -> str:
     """Функцию маскировки номера банковской карты"""
 
-    record_type = ''.join(re.split('[^a-zA-Z]*', card_number))
-    return f"record_type {card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+        return f"record_type {card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(number_account: Union[int]) -> str:
     """Функцию маскировки номера банковского счета"""
 
-    record_type = ''.join(re.split('[^a-zA-Z]*', number_account))
     return f"record_type **{number_account[-4:]}"
--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import Union
 
 from src.masks import get_mask_account, get_mask_card_number
@@ -6,10 +8,11 @@ def mask_account_card(account_card: Union[str]) -> str:
     """Функция принимает один аргумент — строку, возвращает строку с замаскированным номером"""
 
     account = "Счет"
+    record_type = ''.join(re.split('[^a-zA-Z ]*', account_card))
     if account in account_card:
-        return get_mask_account(account_card)
+        return f'{record_type}{get_mask_account(account_card)}'
     else:
-        return get_mask_card_number(account_card)
+        return f'{record_type}{get_mask_card_number(account_card)}'
 
 
 def get_date(date_format: Union[str]) -> str:
