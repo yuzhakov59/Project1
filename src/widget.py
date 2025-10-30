6348591570d28e7d667caf72eaffa40e057d6dca
yuzhakov <yuzhakov.a.a@mail.ru>
2025-10-30 10:51:03 +0500

    widget test

--- src/widget.py
+++ src/widget.py
@@ -11,14 +11,24 @@ def mask_account_card(account_card: Union[str]) -> str:
     """Функция принимает один аргумент — строку, возвращает строку с замаскированным номером"""
 
     account = "Счет"
+    cart_number = re.findall(r"\d+", account_card)[0]
     record_type = ''.join(re.split('[^a-zA-Zа-яА-Я ]*', account_card))
     if account in account_card:
-        return f'{record_type}{get_mask_account(account_card)}'
+        if len(cart_number) == 20:
+            return f'{record_type}{get_mask_account(cart_number)}'
+        else:
+            return "Некорректно введён номер счета"
     else:
-        return f'{record_type}{get_mask_card_number(account_card)}'
+        if len(cart_number) == 16:
+            return f'{record_type}{get_mask_card_number(cart_number)}'
+        else:
+            return "Некорректно введён номер карты"
 
 
 def get_date(date_format: Union[str]) -> str:
     """Функция преобразует дату"""
 
-    return f'{date_format[8:10]}.{date_format[5:7]}.{date_format[0:4]}'
+    if len(date_format) == 26:
+        return f'{date_format[8:10]}.{date_format[5:7]}.{date_format[0:4]}'
+    else:
+        return "Некорректно введёна дата"
