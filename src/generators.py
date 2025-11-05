7d218460b67e5c0962dbf8209e8c874919b1dd1e
yuzhakov <yuzhakov.a.a@mail.ru>
2025-11-05 21:43:15 +0500

    filter_by_currency

---
+++ src/generators.py
@@ -0,0 +1,13 @@
+def filter_by_currency(transactions, currency):
+    while True:
+        for y in transactions:
+            if y["operationAmount"]["currency"]["name"] == currency:
+                yield y
+
+
+x = [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "operationAmount":
+    {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}}, "description": "Перевод организации",
+    "from": "Счет 75106830613657916952", "to": "Счет 11776614605963066702"},
+    {"id": 142264268, "state": "EXECUTED", "date": "2019-04-04T23:20:05.206878", "operationAmount":
+    {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+    "description": "Перевод со счета на счет", "from": "Счет 19708645243227258542", "to": "Счет 75651667383060284188"}]
