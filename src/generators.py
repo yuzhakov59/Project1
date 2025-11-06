2b6144782e1bb03940b9eb07446fe002da2d18c3
yuzhakov <yuzhakov.a.a@mail.ru>
2025-11-06 09:02:22 +0500

    transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,24 @@
-def filter_by_currency(transactions, currency):
+from typing import Iterator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator:
     while True:
         for y in transactions:
-            if y["operationAmount"]["currency"]["name"] == currency:
-                yield y
+            if not y:
+                return "Нет транзакции"
+            else:
+                if y["operationAmount"]["currency"]["code"] == currency:
+                    yield y
+
+
+def transaction_descriptions(transactions: list[dict]) -> str:
+    while True:
+        for y in transactions:
+            if not y:
+                return "Нет транзакции"
+            else:
+                if "description" in y:
+                    yield y["description"]
+
+
+card_number_generator =
