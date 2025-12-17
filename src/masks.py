9834997c6135ca13fade99bb2b8a5ea64677f0ce
yuzhakov <yuzhakov.a.a@mail.ru>
2025-12-17 09:19:34 +0500

    13.1 Библиотеки csv и pandas, доработка №1

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ from typing import Union
 
 logger = logging.getLogger('masksLogger')
 logger.setLevel(logging.DEBUG)  # Записываем всё
-file_handler = logging.FileHandler('../logs/masks.log', 'w', encoding='utf-8')
+file_handler = logging.FileHandler('./logs/masks.log', 'w', encoding='utf-8')
 file_handler.setLevel(logging.DEBUG)
 log_format = logging.Formatter('%(asctime)s – %(name)s – %(levelname)s – %(message)s', datefmt='%H:%M:%S')
 file_handler.setFormatter(log_format)
